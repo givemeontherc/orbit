76f70d87e64fcfdfcac2c59f186d5d142121ac52
givemeontherc <givemeontherc@outlook.com>
2024-08-02 23:26:37 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,30 @@ import pyautogui as pag
 import subprocess
 import time
 
-#import pyautogui as pag
-#import subprocess
-#import time 
+# Code
+exe_path = "vpnplanet.exe"
 
+subprocess.Popen([exe_path])
+
+actions = [
+    (611,528,4),
+    (611,528,2),
+    (611,528,2),
+    (611,528,2),
+    (611,528,2),
+    (611,528,2),
+    (625,371,36),
+    (626,531,5),
+    (500,531,26),
+    (427,479,12),
+    (629,434,6),
+    (856,156,36),
+]
+
+#loop
+
+for x, y, duration in actions:
+    pag.click(x, y, duration=duration)
+
+# end of file here 
+# thank yo 
